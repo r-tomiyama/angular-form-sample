928cdc8b70207320e6951d9822164d8bfb2ff081
r-tomiyama <r.tomiyama@opt.ne.jp>
2021-05-28 15:44:25 +0900

    add: material input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatInputModule} from '@angular/material/input';
 
 import { AppComponent } from './app.component';
 import { ProfileEditorComponent } from './profile-editor/profile-editor.component';
 import { NameEditorComponent } from './name-editor/name-editor.component';
+import { MatFormComponent } from './mat-form/mat-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProfileEditorComponent,
-    NameEditorComponent
+    NameEditorComponent,
+    MatFormComponent
   ],
   imports: [
     BrowserModule,
-    ReactiveFormsModule
+    NoopAnimationsModule,
+    ReactiveFormsModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
